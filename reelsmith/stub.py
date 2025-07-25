02a01ebba88075bc486071ef5144932976cc5911
Harikeshav R <r.harikeshav@gmail.com>
2025-07-25 10:44:36 -0400

    Set default values for optional fields in `State` model for consistency

--- reelsmith/stub.py
+++ reelsmith/stub.py
@@ -7,8 +7,8 @@ from pydantic import BaseModel, Field
 class State(BaseModel):
     topic: str = Field(description="The topic for the video script.")
     search_summary: Optional[list[str]] = Field(
-        description="The summary of the research performed by searching the web.")
-    script: Optional[str] = Field(description="The generated script.")
-    audio_path: Optional[Path] = Field(description="The path to the generated audio.")
-    caption_path: Optional[Path] = Field(description="The path to the generated captions.")
-    video_path: Optional[Path] = Field(description="The path to the final generated video.")
+        description="The summary of the research performed by searching the web.", default=None)
+    script: Optional[str] = Field(description="The generated script.", default=None)
+    audio_path: Optional[Path] = Field(description="The path to the generated audio.", default=None)
+    caption_path: Optional[Path] = Field(description="The path to the generated captions.", default=None)
+    video_path: Optional[Path] = Field(description="The path to the final generated video.", default=None)
